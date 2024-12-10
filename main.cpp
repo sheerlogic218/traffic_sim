e4d48079d53001074eb599c3f6e86c3a7095db00
Edward Baker <ed9baker@outlook.com>
2024-12-10 18:56:00 +0000

    Dot can move

--- main.cpp
+++ main.cpp
@@ -1,22 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+int getRandomNumber(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // Create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    window.setFramerateLimit(60);
 
+    sf::CircleShape shape(10.f);
+    
+    // Run the program as long as the window is open
     while (window.isOpen())
     {
+        // Check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
+        // Clear the window with black colour
+        window.clear(sf::Color::Black);
+
+		float x = getRandomNumber(0, 800);
+		float y = getRandomNumber(0, 600);
+
+		cout << "x: " << x << " y: " << y << endl;
+        
+		shape.setPosition(x, y);
+
         window.draw(shape);
+
+        // End the current frame
         window.display();
     }
 
